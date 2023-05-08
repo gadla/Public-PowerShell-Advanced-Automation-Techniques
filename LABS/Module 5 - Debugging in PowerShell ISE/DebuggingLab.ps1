933adad0d4aab5ee12cc1fb17905b1be53d288dc
Gadi Lev-Ari <gadla@hotmail.com>
2023-05-08 17:55:53 +0300

    'Lab 5 - Debugging in Powershell ISE' added

---
+++ LABS/Module 5 - Debugging in PowerShell ISE/DebuggingLab.ps1
@@ -0,0 +1,16 @@
+# This script contains some simple PowerShell commands that we will use for debugging practice.
+
+function  Demo-Function {
+    param (
+        [int]$Repetition = 2
+    )
+    Write-Output "This is a demo function"
+    while ($Repetition -gt 0) {
+        Write-Output "This is repetition number $Repetition" 
+        $Repetition--       
+    }
+}
+
+Write-Host "This is a demo script" -ForegroundColor Green -BackgroundColor Black
+Write-Host "This is a another command in yourdemo script" -ForegroundColor Green -BackgroundColor Black
+Get-Demo -Repetition 2
